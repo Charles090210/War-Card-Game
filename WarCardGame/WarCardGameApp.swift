78cfc90fd5490320752a6c7c7c67fdd4ab4f6733
Charles <zpiao@ascensionacademy.org>
2026-02-10 08:51:59 -0600

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Charles Piao on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
