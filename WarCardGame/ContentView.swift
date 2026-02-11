9a9f7b153be76dc865c6888898bc14727c90402f
Charles <zpiao@ascensionacademy.org>
2026-02-11 09:29:58 -0600

    Final Game

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -8,14 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "back"
+    @State private var computerCard = "back"
+    @State private var playerScore = 0
+    @State private var computerScore = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.backgroundPlain)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image(.logo)
+                Spacer()
+                HStack {
+                    Image(playerCard)
+                    Image(computerCard)
+                }
+                Spacer()
+                
+                Button {
+                    // Call a function to deal the cards
+                    dealCards()
+                } label: {
+                    Image("button")
+                }
+                .foregroundStyle(Color.black)
+                
+                Spacer()
+                
+                // Scores
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Computer")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(computerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    // MARK: Functions
+    
+    func dealCards(){
+        // 1. Randomize the card values
+        let playerValue = Int.random(in: 2...14)
+        let computerValue = Int.random(in: 2...14)
+        
+        // 2. Update card images
+        playerCard = "card\(playerValue)"
+        computerCard = "card\(computerValue)"
+        // 3. Calculate the score
+        if playerValue > computerValue{
+            playerScore += 1
+        }else if computerValue > playerValue{
+            computerScore += 1
+        } else {
+            playerScore += 1
+            computerScore += 1
+        }
     }
 }
 
